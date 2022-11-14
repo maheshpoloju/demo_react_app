d7f760f4771e1be7b5d099a7a834a2ab5ed4fcd8
maheshpoloju <polojumahesh3@gmail.com>
2022-11-14 18:44:58 +0530

    images modified

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,24 @@
 import { useState } from "react";
-import doc1 from "./doc1.jpg";
-import doc2 from "./doc2.png";
-import doc3 from "./doc3.jpg";
+
 import "./index.css";
 const teamData = [
   {
     id: 1,
-    imageUrl: doc2,
+    imageUrl: "https://www.freeiconspng.com/uploads/profile-icon-9.png",
     description:
-      "Srila Prabhupada was born Abhay Charan De on September 1, 1896 to a practicing Hindu family in Kolkata.  As a youth growing up in British-controlled India, Abhay became involved with Mahatma Gandhi’s civil disobedience movement to secure independence for his nation. A 1922 meeting with a prominent scholar and religious leader, Srila Bhaktisiddhanta Sarasvati, proved most influential on Abhay’s future calling. Srila Bhaktisiddhanta was a leader in the Gaudiya Vaishnava denomination, a monotheistic tradition within the broad Hindu culture. He asked Abhay to bring the teachings of Lord Krishna to the English-speaking world. Srila Prabhupada’s most significant contributions, perhaps, are his books. He authored over 70 volumes on the Krishna tradition, which are highly respected by scholars for their authority, depth, fidelity to the tradition and clarity. Several of his works are used as textbooks in university courses. His writings have been translated into 76 languages. His most prominent works include: Bhagavad-gita As It Is, the multi-volume Srimad-Bhagavatam and the set of Sri Caitanya-caritamrita books.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
   {
     id: 2,
-    imageUrl: doc3,
+    imageUrl: "https://www.freeiconspng.com/uploads/profile-icon-9.png",
     description:
-      "His Grace Vaisesika Prabhu was born in 1956. As a teen, he was an avid spiritual seeker. Soon after receiving a Back to Godhead magazine from a friend in the spring of 1973, he moved into the ISKCON temple in San Francisco at the age of sixteen and, with the blessings of his parents, became a disciple of Srila Prabhupada and a fulltime monk dedicated to the practice of Krishna bhakti.",
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. A piece of classical Latin from  5 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum(The Extremes of Good and Evil) by Cicero, written in 45 BC.",
   },
   {
     id: 3,
-    imageUrl: doc1,
+    imageUrl: "https://www.freeiconspng.com/uploads/profile-icon-9.png",
     description:
-      "Hare Krishna, My name is Paavan Sarovar Das. I am a Board Certified  Family Medicine physician. I have been actively practicing physicianin in the field of medicine for over 22 years. During the past few years, I served as Medical Director at West Virginia University Urgent care. I witnessed the effects and outcomes of the pandemic very closely. The pandemic has recently caused people to feel the necessity of medical care in the society in general. With this website, I want to serve the very dear disciples of Srila Prabhupada. This website and this service is my offering to Srila Prabhupada and my dearest Gurumaharaj.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   },
 ];
 
@@ -41,7 +39,11 @@ const Home = () => {
           return (
             <div className="person-container" key={each.id}>
               <div className="image-container">
-                <img className="profile-image" src={each.imageUrl} />
+                <img
+                  alt="docImg"
+                  className="profile-image"
+                  src={each.imageUrl}
+                />
               </div>
               <p className="description">
                 {selected === each.id ? each.description : lessDescription}
