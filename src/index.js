6acab6ab01cd9b003c3373a4fc8b20ab06f3bcf8
maheshpoloju <polojumahesh3@gmail.com>
2025-06-08 19:54:56 +0530

    homepage removed-package json

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 
+// add in package.json
+// "homepage": "https://maheshpoloju.github.io/demo_react_app",
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
